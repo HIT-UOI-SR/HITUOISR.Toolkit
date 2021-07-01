c518bee56af0eb3d655e2fc871953c19ed05390b
miroox <miroox@outlook.com>
2021-07-01 16:32:09 +0800

    simple temporary file

---
+++ HITUOISR.Toolkit.Common/TemporaryFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace HITUOISR.Toolkit.Common
+{
+    /// <summary>
+    /// 简易临时文件。
+    /// </summary>
+    public sealed class TemporaryFile : IDisposable
+    {
+        /// <summary>
+        /// 创建临时文件。
+        /// </summary>
+        public TemporaryFile() : this(Path.GetTempPath()) { }
+
+        /// <summary>
+        /// 在指定文件夹创建临时文件。
+        /// </summary>
+        /// <param name="directory">临时文件所在文件夹。</param>
+        public TemporaryFile(string directory)
+        {
+            File = new(Path.Combine(directory, Path.GetRandomFileName()));
+            File.Create().Dispose();
+        }
+
+        /// <summary>
+        /// 临时文件。
+        /// </summary>
+        public FileInfo File { get; }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (File.Exists) File.Delete();
+        }
+    }
+}
