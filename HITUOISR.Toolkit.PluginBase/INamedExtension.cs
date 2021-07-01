0a79d03910b183f48974b26a1b14e3f0bc5629be
miroox <miroox@outlook.com>
2021-07-01 11:15:50 +0800

    add interfaces

---
+++ HITUOISR.Toolkit.PluginBase/INamedExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HITUOISR.Toolkit.PluginBase
+{
+    /// <summary>
+    /// 具名扩展接口。
+    /// </summary>
+    public interface INamedExtension
+    {
+        /// <summary>
+        /// 扩展名称。
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// 唯一标识。
+        /// </summary>
+        Guid Guid { get; }
+    }
+}
---
+++ HITUOISR.Toolkit.PluginBase/IPluginManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HITUOISR.Toolkit.PluginBase
+{
+    /// <summary>
+    /// 插件管理器接口。
+    /// </summary>
+    public interface IPluginManager : IServiceProvider, IDisposable, IAsyncDisposable
+    {
+        /// <summary>
+        /// 枚举插件。
+        /// </summary>
+        IEnumerable<IPlugin> Plugins { get; }
+    }
+}
