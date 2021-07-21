9008cc24ea065ff9307021a17de3e9d72bbc1027
miroox <miroox@outlook.com>
2021-07-21 23:10:23 +0800

    more test cases for settings model

---
+++ HITUOISR.Toolkit.Settings.Model.Tests/ConfigureSettingsModelTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HITUOISR.Toolkit.Settings.Model.Tests
+{
+    public class ConfigureSettingsModelTests
+    {
+        private class SampleModel : ObservableObject
+        {
+            public int Value
+            {
+                get => _Value;
+                set => SetProperty(ref _Value, value);
+            }
+            private int _Value;
+        }
+
+        private class FakeSettings : ISettings
+        {
+            public IEnumerable<ISettingsSection> GetChildren() => Array.Empty<ISettingsSection>();
+            public ISettingsSection GetSection(string key) => throw new NotImplementedException();
+            public T GetValue<T>(string key) => throw new NotImplementedException();
+            public T GetValueOrElse<T>(string key, Func<T> defaultGetter) => defaultGetter();
+            public void SetValue<T>(string key, T value) => throw new NotImplementedException();
+        }
+
+        [Fact()]
+        public void Configure_InitOnly_AsExpecterd()
+        {
+            var inited = false;
+            var tracked = false;
+            void Initializer(SampleModel model, ISettings settings) => inited = true;
+            void Tracker(ISettings settings, SampleModel model, string? prop) => tracked = true;
+            ConfigureSettingsModel<SampleModel> configure = new(Initializer, Tracker);
+            FakeSettings settings = new();
+            SampleModel model = new();
+            configure.Configure(settings, model, bidirection: false);
+            Assert.True(inited, "Configure doesnot initialize");
+            model.Value = 1;
+            Assert.False(tracked, "Configure setup tracked on init-only mode.");
+        }
+
+        [Fact()]
+        public void Configure_Bidirection_AsExpecterd()
+        {
+            var inited = false;
+            string? propName = null;
+            void Initializer(SampleModel model, ISettings settings) => inited = true;
+            void Tracker(ISettings settings, SampleModel model, string? prop) => propName = prop;
+            ConfigureSettingsModel<SampleModel> configure = new(Initializer, Tracker);
+            FakeSettings settings = new();
+            SampleModel model = new();
+            configure.Configure(settings, model, bidirection: true);
+            Assert.True(inited, "Configure doesnot initialize");
+            model.Value = 1;
+            Assert.Equal(nameof(SampleModel.Value), propName);
+        }
+    }
+}
---
+++ HITUOISR.Toolkit.Settings.Model.Tests/DummySettingsModelTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace HITUOISR.Toolkit.Settings.Model.Tests
+{
+    public class DummySettingsModelTests
+    {
+        private class SampleModel : ObservableObject, IEquatable<SampleModel>
+        {
+            public double Value
+            {
+                get => _Value;
+                set => SetProperty(ref _Value, value);
+            }
+            private double _Value = double.NaN;
+
+            public bool Equals([AllowNull] SampleModel other) => other != null && Value.CompareTo(other.Value) == 0;
+            public override bool Equals(object? obj) => Equals(obj as SampleModel);
+            public override int GetHashCode() => HashCode.Combine(Value);
+        }
+
+        private static TheoryData<INotifyPropertyChanged> ModelsData() => new()
+        {
+            new SampleModel(),
+            new SampleModel { Value = double.Epsilon },
+        };
+
+        [Fact()]
+        public void Ctor_New_AsExpected()
+        {
+            DummySettingsModel<SampleModel> settingsModel = new();
+            Assert.Equal(new SampleModel(), settingsModel.Value);
+        }
+
+        [Theory]
+        [MemberData(nameof(ModelsData))]
+        public void Ctor_SameValue_AsExpected<TModel>(TModel model) where TModel : class, INotifyPropertyChanged, new()
+        {
+            DummySettingsModel<TModel> settingsModel = new(model);
+            Assert.Same(model, settingsModel.Value);
+        }
+    }
+}
