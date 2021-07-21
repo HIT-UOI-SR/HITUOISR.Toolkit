c62d284d775124d220efc0f9ff232b93d82080e3
miroox <miroox@outlook.com>
2021-07-21 21:55:43 +0800

    add embedded json settings file test cases

--- HITUOISR.Toolkit.Settings.Json.Tests/JsonFileSettingsProviderTests.cs
+++ HITUOISR.Toolkit.Settings.Json.Tests/JsonFileSettingsProviderTests.cs
@@ -1,5 +1,6 @@
 ﻿using HITUOISR.Toolkit.Common;
 using HITUOISR.Toolkit.Common.Comparers;
+using HITUOISR.Toolkit.Settings.FileBase;
 using Microsoft.Extensions.FileProviders;
 using System;
 using System.Diagnostics.Contracts;
@@ -15,7 +16,9 @@ namespace HITUOISR.Toolkit.Settings.Json.Tests
     {
         private static readonly string BaseDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
         private static readonly string PhysicalFileName = "file-settings.json";
-        private static TheoryData<string, object> LoadExistsTestData() => new()
+        private static readonly string EmbeddedFileName = "embedded-settings.json";
+
+        private static TheoryData<string, object> PhysicalExistsTestData() => new()
         {
             {
                 "num",
@@ -39,9 +42,33 @@ namespace HITUOISR.Toolkit.Settings.Json.Tests
             }
         };
 
+        private static TheoryData<string, object> EmbededdExistsTestData() => new()
+        {
+            {
+                "num",
+                42
+            },
+            {
+                "num.real",
+                -1.7E308
+            },
+            {
+                "text.lipsum",
+                "Quisque consectetur finibus arcu"
+            },
+            {
+                "point",
+                new Point(7, -5)
+            },
+            {
+                "array.real",
+                new[] { double.NaN, Math.E }
+            }
+        };
+
         [Theory]
-        [MemberData(nameof(LoadExistsTestData))]
-        public void TryLoad_Exists<T>(string key, T expected)
+        [MemberData(nameof(PhysicalExistsTestData))]
+        public void TryLoad_Physical_Exists<T>(string key, T expected)
         {
             JsonFileSettingsSource source = new(PhysicalFileName)
             {
@@ -57,14 +84,46 @@ namespace HITUOISR.Toolkit.Settings.Json.Tests
         }
 
         [Theory]
+        [MemberData(nameof(EmbededdExistsTestData))]
+        public void TryLoad_Embededd_Exists<T>(string key, T expected)
+        {
+            JsonFileSettingsSource source = new(EmbeddedFileName)
+            {
+                FileProvider = new EmbeddedFileProvider(typeof(JsonFileSettingsProviderTests).Assembly),
+            };
+            JsonFileSettingsProvider provider = new(source);
+            SettingsKeyInfo keyInfo = new(key, typeof(T));
+            Assert.True(provider.TryLoad(keyInfo), "TryLoad failed.");
+            Assert.True(provider.TryGet(key, out var value), "TryGet failed.");
+            Assert.IsType<T>(value);
+            Contract.Assume(value is T);
+            Assert.Equal(expected, (T)value);
+        }
+
+        [Theory]
         [InlineData("")]
         [InlineData("text")]
         [InlineData(".lipsum")]
-        public void TryLoad_NotExists(string key)
+        public void TryLoad_Physical_NotExists(string key)
         {
             JsonFileSettingsSource source = new(PhysicalFileName)
             {
                 FileProvider = new PhysicalFileProvider(BaseDirectory),
+            };
+            JsonFileSettingsProvider provider = new(source);
+            SettingsKeyInfo keyInfo = new(key, typeof(object));
+            Assert.False(provider.TryLoad(keyInfo), "TryLoad failed.");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("text")]
+        [InlineData(".lipsum")]
+        public void TryLoad_Embededd_NotExists(string key)
+        {
+            JsonFileSettingsSource source = new(EmbeddedFileName)
+            {
+                FileProvider = new EmbeddedFileProvider(typeof(JsonFileSettingsProviderTests).Assembly),
             };
             JsonFileSettingsProvider provider = new(source);
             SettingsKeyInfo keyInfo = new(key, typeof(object));
@@ -107,7 +166,7 @@ namespace HITUOISR.Toolkit.Settings.Json.Tests
 
         [Theory]
         [MemberData(nameof(SaveTestData))]
-        public void Save_AfterModified<T>(string key, T value, string expectedJsonText)
+        public void Save_Physical_AfterModified<T>(string key, T value, string expectedJsonText)
         {
             using var file = CreateTempFile();
             JsonFileSettingsSource source = new(file.File.Name)
@@ -129,6 +188,36 @@ namespace HITUOISR.Toolkit.Settings.Json.Tests
             using var expecteddoc = JsonDocument.Parse(expectedJsonText, jsonDocumentOptions);
             var expectedJson = expecteddoc.RootElement;
             Assert.Equal(expectedJson, fulldoc.RootElement.GetProperty(key), new JsonElementEqualityComparer(StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Fact()]
+        public void Save_Embededd_NotSupport_Throws()
+        {
+            JsonFileSettingsSource source = new(EmbeddedFileName)
+            {
+                FileProvider = new EmbeddedFileProvider(typeof(JsonFileSettingsProviderTests).Assembly),
+            };
+            JsonFileSettingsProvider provider = new(source);
+            Assert.Throws<NotSupportedException>(() => provider.Save());
+        }
+
+        [Fact()]
+        public void Save_Embededd_NotSupport_SetHandler()
+        {
+            var handled = false;
+            JsonFileSettingsSource source = new(EmbeddedFileName)
+            {
+                FileProvider = new EmbeddedFileProvider(typeof(JsonFileSettingsProviderTests).Assembly),
+                ExceptionHandler = (FileExceptionContext ctx) =>
+                {
+                    Assert.IsType<NotSupportedException>(ctx.Exception);
+                    ctx.Ignore = true;
+                    handled = true;
+                }
+            };
+            JsonFileSettingsProvider provider = new(source);
+            Assert.False(provider.Save());
+            Assert.True(handled);
         }
 
         private static TemporaryFile CreateTempFile()
