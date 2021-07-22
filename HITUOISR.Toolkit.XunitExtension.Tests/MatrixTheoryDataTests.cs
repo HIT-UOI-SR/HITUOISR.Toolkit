28e36c0644fabd8e8bb8e76e9dddfd30b7073b47
miroox <miroox@outlook.com>
2021-07-22 19:50:28 +0800

    add missing test cases for MatrixTheoryData

--- HITUOISR.Toolkit.XunitExtension.Tests/MatrixTheoryDataTests.cs
+++ HITUOISR.Toolkit.XunitExtension.Tests/MatrixTheoryDataTests.cs
@@ -62,6 +62,21 @@ namespace HITUOISR.Toolkit.XunitExtension.Tests
         }
 
         [Fact()]
+        public void Ctor_Theory_1x1x1()
+        {
+            TheoryData<int> v1 = new() { 1, 2 };
+            TheoryData<int> v2 = new() { 2, 3 };
+            TheoryData<int> v3 = new() { 3, 4 };
+            MatrixTheoryData<int, int, int> m = new(v1, v2, v3);
+            Assert.Equal(new[] {
+                (1, 2, 3), (1, 2, 4),
+                (1, 3, 3), (1, 3, 4),
+                (2, 2, 3), (2, 2, 4),
+                (2, 3, 3), (2, 3, 4) },
+                m.AsValueTuples());
+        }
+
+        [Fact()]
         public void Ctor_Theory_1x2()
         {
             TheoryData<int> v1 = new() { 1, 2 };
