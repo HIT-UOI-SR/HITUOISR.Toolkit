8d1f0a6c0b4952e2d8ada614e09c3159631f2aed
miroox <miroox@outlook.com>
2021-07-22 21:05:19 +0800

    more test cases for settings

---
+++ HITUOISR.Toolkit.Settings.Tests/SettingsBuilderExtensionsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace HITUOISR.Toolkit.Settings.Tests
+{
+    public class SettingsBuilderExtensionsTests
+    {
+        [Fact()]
+        public void GetSerices_WithoutSet_Null()
+        {
+            SettingsBuilder builder = new();
+            Assert.Null(builder.GetServices());
+        }
+
+        [Fact()]
+        public void SetServices_GetSerices_AsExpected()
+        {
+            ServiceCollection services = new();
+            SettingsBuilder builder = new();
+            builder.SetServices(services);
+            Assert.Equal(services, builder.GetServices());
+        }
+
+        [Theory]
+        [InlineData("num", 0, false)]
+        [InlineData("key", "num", true)]
+        public void RegisterKey_AsExpected<T>(string path, T defValue, bool isReadonly) where T : notnull
+        {
+            SettingsBuilder builder = new();
+            builder.RegisterKey(path, defValue, isReadonly);
+            Assert.Collection(builder.SettingsKeys,
+                key =>
+                {
+                    Assert.Equal(path, key.Path);
+                    Assert.Equal(typeof(T), key.Type);
+                    Assert.Equal(defValue, key.DefaultValue);
+                    Assert.Equal(isReadonly, key.IsReadOnly);
+                });
+        }
+    }
+}
---
+++ HITUOISR.Toolkit.Settings.Tests/SettingsBuilderTests.cs
@@ -0,0 +1,85 @@
+﻿using HITUOISR.Toolkit.XunitExtension;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace HITUOISR.Toolkit.Settings.Tests
+{
+    public class SettingsBuilderTests
+    {
+        private static TheoryData<SettingsKeyInfo[]> SettingsKeysData() => new()
+        {
+            new SettingsKeyInfo[]
+            {
+                new("key", typeof(string)),
+                new("num", typeof(double)) { DefaultValue = double.PositiveInfinity },
+            },
+            new SettingsKeyInfo[]
+            {
+                new("key", typeof(string)) { DefaultValue = string.Empty },
+                new("num.int", typeof(int)) { IsReadOnly = true },
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(SettingsKeysData))]
+        public void RegisterKey_AsExpected(IEnumerable<ISettingsKeyInfo> keys)
+        {
+            SettingsBuilder builder = new();
+            foreach (var key in keys)
+            {
+                builder.RegisterKey(key);
+            }
+            Assert.Equal(keys, builder.SettingsKeys);
+        }
+
+        private static TheoryData<ISettingsKeyInfo> DuplicateKeyData() => new()
+        {
+            new SettingsKeyInfo("num", typeof(int)),
+            new SettingsKeyInfo("num", typeof(double)),
+            new SettingsKeyInfo("NUM", typeof(double)),
+        };
+
+        [Theory]
+        [MemberData(nameof(DuplicateKeyData))]
+        public void RegisterKey_Exists_Throw(ISettingsKeyInfo key)
+        {
+            SettingsBuilder builder = new();
+            builder.RegisterKey(new SettingsKeyInfo("num", typeof(int)));
+            Assert.Throws<SettingsKeyExistsException>(() => builder.RegisterKey(key));
+        }
+
+        private static TheoryData<ISettingsSource[]> SourcesData() => new()
+        {
+            new ISettingsSource[]
+            {
+                new Memory.MemorySettingsSource(new Dictionary<string, object?>()),
+            },
+            new ISettingsSource[]
+            {
+                new Memory.MemorySettingsSource(new Dictionary<string, object?>()),
+                new Memory.MemorySettingsSource(new Dictionary<string, object?>(){ ["key"] = "build" }),
+            },
+        };
+
+        private static MatrixTheoryData<SettingsKeyInfo[], ISettingsSource[]> BuildTestData() => new(SettingsKeysData(), SourcesData());
+
+        [Theory]
+        [MemberData(nameof(BuildTestData))]
+        public void Build_AsExpected(IEnumerable<ISettingsKeyInfo> keys, IEnumerable<ISettingsSource> sources)
+        {
+            SettingsBuilder builder = new();
+            foreach (var key in keys)
+            {
+                builder.RegisterKey(key);
+            }
+            foreach (var src in sources)
+            {
+                builder.Sources.Add(src);
+            }
+            var settings = builder.Build();
+            Assert.Equal(keys, settings.Keys);
+            Assert.Equal(sources.Count(), settings.Providers.Count());
+        }
+    }
+}
