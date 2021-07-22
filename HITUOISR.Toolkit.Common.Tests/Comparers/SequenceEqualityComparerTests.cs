240716a6482a9e286861d5d7d63af83b258fa621
miroox <miroox@outlook.com>
2021-07-22 22:16:50 +0800

    more test cases for comparers

---
+++ HITUOISR.Toolkit.Common.Tests/Comparers/SequenceEqualityComparerTests.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace HITUOISR.Toolkit.Common.Comparers.Tests
+{
+    public class SequenceEqualityComparerTests
+    {
+        private static TheoryData<int[], List<int>> IntEqualData() => new()
+        {
+            {
+                new[] { 1, 2, 3 },
+                new() { 1, 2, 3 }
+            },
+        };
+
+        private static TheoryData<string[], List<string>> StringEqualData() => new()
+        {
+            {
+                new[] { "AB", "2", "#" },
+                new() { "AB", "2", "#" }
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntEqualData))]
+        [MemberData(nameof(StringEqualData))]
+        public void Equal_Default<TElement>(IEnumerable<TElement> s1, IEnumerable<TElement> s2)
+        {
+            SequenceEqualityComparer<TElement> comparer = new();
+            Assert.True(comparer.Equals(s1, s2));
+            Assert.Equal(comparer.GetHashCode(s1), comparer.GetHashCode(s2));
+        }
+
+        private static TheoryData<int[], int[]> IntNotEqualData() => new()
+        {
+            {
+                new[] { 1, 2, 3 },
+                new[] { 1, -2, 3 }
+            },
+        };
+
+        private static TheoryData<string[], string[]> StringNotEqualData() => new()
+        {
+            {
+                new[] { "AB", "2", "#" },
+                new[] { "aB", " 2", "#" }
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntNotEqualData))]
+        [MemberData(nameof(StringNotEqualData))]
+        public void NotEqual_Default<TElement>(IEnumerable<TElement> s1, IEnumerable<TElement> s2)
+        {
+            SequenceEqualityComparer<TElement> comparer = new();
+            Assert.False(comparer.Equals(s1, s2));
+            Assert.NotEqual(comparer.GetHashCode(s1), comparer.GetHashCode(s2));
+        }
+
+        private class AbsEqualityComparer : EqualityComparer<int>
+        {
+            public override bool Equals([AllowNull] int x, [AllowNull] int y)
+            {
+                try
+                {
+                    return Math.Abs(x) == Math.Abs(y);
+                }
+                catch (OverflowException)
+                {
+                    return false;
+                }
+            }
+
+            public override int GetHashCode([DisallowNull] int obj)
+            {
+                try
+                {
+                    return Math.Abs(obj);
+                }
+                catch (OverflowException)
+                {
+                    return obj;
+                }
+            }
+        }
+
+        private static TheoryData<int[], List<int>, IEqualityComparer<int>> IntComparerEqualData() => new()
+        {
+            {
+                new[] { 1, 2, 3 },
+                new() { 1, -2, 3 },
+                new AbsEqualityComparer()
+            },
+        };
+
+        private static TheoryData<string[], List<string>, IEqualityComparer<string>> StringComparerEqualData() => new()
+        {
+            {
+                new[] { "AB", "2", "#" },
+                new() { "aB", "2", "#" },
+                StringComparer.OrdinalIgnoreCase
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntComparerEqualData))]
+        [MemberData(nameof(StringComparerEqualData))]
+        public void Equal_WithComparer<TElement>(IEnumerable<TElement> s1, IEnumerable<TElement> s2, IEqualityComparer<TElement> elemComparer)
+        {
+            SequenceEqualityComparer<TElement> comparer = new(elemComparer);
+            Assert.True(comparer.Equals(s1, s2));
+            Assert.Equal(comparer.GetHashCode(s1), comparer.GetHashCode(s2));
+        }
+
+        private static TheoryData<int[], int[], IEqualityComparer<int>> IntComparerNotEqualData() => new()
+        {
+            {
+                new[] { 1, 2, int.MaxValue },
+                new[] { 1, 2, int.MinValue },
+                new AbsEqualityComparer()
+            },
+        };
+
+        private static TheoryData<string[], string[], IEqualityComparer<string>> StringComparerNotEqualData() => new()
+        {
+            {
+                new[] { "AB", "贰", "#" },
+                new[] { "aB", "二", "#" },
+                StringComparer.OrdinalIgnoreCase
+            },
+            {
+                new[] { "AB", "2", "#" },
+                new[] { "aB", " 2", "#" },
+                StringComparer.OrdinalIgnoreCase
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntComparerNotEqualData))]
+        [MemberData(nameof(StringComparerNotEqualData))]
+        public void NotEqual_WithComparer<TElement>(IEnumerable<TElement> s1, IEnumerable<TElement> s2, IEqualityComparer<TElement> elemComparer)
+        {
+            SequenceEqualityComparer<TElement> comparer = new(elemComparer);
+            Assert.False(comparer.Equals(s1, s2));
+            Assert.NotEqual(comparer.GetHashCode(s1), comparer.GetHashCode(s2));
+        }
+    }
+}
---
+++ HITUOISR.Toolkit.Common.Tests/Comparers/SetEqualityComparerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace HITUOISR.Toolkit.Common.Comparers.Tests
+{
+    public class SetEqualityComparerTests
+    {
+        private static TheoryData<HashSet<int>, SortedSet<int>> IntEqualTestData() => new()
+        {
+            {
+                new() { 1, 4, 3 },
+                new() { 3, 1, 4 }
+            },
+        };
+
+        private static TheoryData<HashSet<string>, SortedSet<string>> StringEqualTestData() => new()
+        {
+            {
+                new() { "a", "Ab", "#" },
+                new() { "Ab", "#", "a" }
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntEqualTestData))]
+        [MemberData(nameof(StringEqualTestData))]
+        public void Equal<T>(ISet<T> s1, ISet<T> s2)
+        {
+            SetEqualityComparer<T> comparer = new();
+            Assert.True(comparer.Equals(s1, s2));
+            Assert.Equal(comparer.GetHashCode(s1), comparer.GetHashCode(s2));
+        }
+
+        private static TheoryData<HashSet<int>, SortedSet<int>> IntNotEqualTestData() => new()
+        {
+            {
+                new() { 1, 4, 3 },
+                new() { 3, 1, -4 }
+            },
+        };
+
+        private static TheoryData<HashSet<string>, SortedSet<string>> StringNotEqualTestData() => new()
+        {
+            {
+                new() { "a", "Ab", "#" },
+                new() { "AB", "#", "A" }
+            },
+        };
+
+        [Theory]
+        [MemberData(nameof(IntNotEqualTestData))]
+        [MemberData(nameof(StringNotEqualTestData))]
+        public void NotEqual<T>(ISet<T> s1, ISet<T> s2)
+        {
+            SetEqualityComparer<T> comparer = new();
+            Assert.False(comparer.Equals(s1, s2));
+            Assert.NotEqual(comparer.GetHashCode(s1), comparer.GetHashCode(s2));
+        }
+    }
+}
