4f3fa47e4bdde231058f933edebc435d8bbbc631
miroox <miroox@outlook.com>
2021-08-16 00:12:23 +0800

    improve LinearInterpolation tests

--- HITUOISR.Toolkit.Maths/tests/LinearInterpolationTests.cs
+++ HITUOISR.Toolkit.Maths/tests/LinearInterpolationTests.cs
@@ -5,9 +5,20 @@ namespace CovRNADetect.Maths.Tests
 {
     public class LinearInterpolationTests
     {
+        private static object[][] InsufficientPointsData() => new object[][]
+        {
+            Array.Empty<object>(),
+            new object[] { (0D, 0D) },
+        };
+
+        [Theory]
+        [MemberData(nameof(InsufficientPointsData))]
+        public void Ctor_InsufficientPoint_Throws(params (double, double)[] points) =>
+            Assert.Throws<ArgumentException>(() => new LinearInterpolation(points));
+
         [Theory]
         [ClassData(typeof(LinearInterpolationTestData))]
-        public void ApplyTest((double, double)[] points, double x, double y)
+        public void Apply_AsExpected((double, double)[] points, double x, double y)
         {
             LinearInterpolation interpolation = new(points);
             Assert.Equal(interpolation.Apply(x), y, 8);
@@ -15,7 +26,7 @@ namespace CovRNADetect.Maths.Tests
 
         [Theory]
         [ClassData(typeof(LinearInterpolationTestData))]
-        public void ConvertFuncTest((double, double)[] points, double x, double y)
+        public void ConvertFunc_AsExpected((double, double)[] points, double x, double y)
         {
             Func<double, double> interpolation = new LinearInterpolation(points);
             Assert.Equal(interpolation(x), y, 8);
